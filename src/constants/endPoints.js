b57df974b1e0d89da2addf8eb7e6963c226383db
ranganathnk <ranganathnkulkarni@gmail.com>
2022-12-21 18:18:44 +0530

    updated

--- src/constants/endPoints.js
+++ src/constants/endPoints.js
@@ -1,8 +1,9 @@
 // export const NOTES_API = "http://localhost:3001/notes/";
 // export const USERS_API = "http://localhost:3002/users/";
 
- export const NOTES_API = "http://localhost/php-api/getNotes.php";
- export const ADD_NOTE_API = "http://localhost/php-api/addNote.php";
+//export const NOTES_API = "http://localhost/php-api/getNotes.php";
+//export const ADD_NOTE_API = "http://localhost/php-api/addNote.php";
 //  export const USERS_API = "http://localhost/php-api/userLogin.php";
 
-// export const NOTES_API ="https://rnk-projects.000webhostapp.com/php-api/getNotes.php";
+export const NOTES_API ="https://rnk-projects.000webhostapp.com/php-api/getNotes.php";
+export const ADD_NOTE_API ="https://rnk-projects.000webhostapp.com/php-api/addNote.php";
