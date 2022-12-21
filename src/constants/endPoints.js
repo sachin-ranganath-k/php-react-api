01506fdc42384d961541d99a56ba15ec083b2acf
ranganathnk <ranganathnkulkarni@gmail.com>
2022-12-21 17:36:37 +0530

    updated

--- src/constants/endPoints.js
+++ src/constants/endPoints.js
@@ -3,6 +3,6 @@
 
 // export const NOTES_API = "http://localhost/php-api/getNotes.php";
 // export const ADD_NOTE_API = "http://localhost/php-api/addNote.php";
-// export const USERS_API = "http://localhost/php-api/userLogin.php";
+ export const USERS_API = "http://localhost/php-api/userLogin.php";
 
 export const NOTES_API ="https://rnk-projects.000webhostapp.com/php-api/getNotes.php";
