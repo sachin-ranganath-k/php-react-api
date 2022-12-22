210fcc15fbac9896bed1d26d72213c86aaf3671d
ranganathnk <ranganathnkulkarni@gmail.com>
2022-12-22 16:18:13 +0530

    updated

--- src/components/todo/TodoEntryNormal.js
+++ src/components/todo/TodoEntryNormal.js
@@ -1,14 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addNote,
-  getNotes,
-  showError,
-  updateNote,
-} from "../../actions/actions";
 import Loader from "./Loader";
-import { ADD_NOTE_API, NOTES_API } from "../../constants/endPoints";
+import {
+  ADD_NOTE_API,
+  DELETE_NOTE_API,
+  NOTES_API,
+} from "../../constants/endPoints";
 import NavBarUser from "../../navbar/NavbarUser";
 
 const TodoEntryNormal = () => {
@@ -79,6 +76,21 @@ const TodoEntryNormal = () => {
       });
   };
 
+  const deleteNote = (todo_Id) => {
+    setLoading(true);
+    axios
+      .delete(`${DELETE_NOTE_API}?todoId=${todo_Id}`)
+      .then((res) => {
+        setLoading(false);
+        getData();
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        getData();
+      });
+  };
+
   //   const handleUpdatingNote = (e) => {
   //     setUpdatingNote(e.target.value);
   //   };
@@ -129,36 +141,27 @@ const TodoEntryNormal = () => {
               </div>
               <div class="panel-body">
                 <div className="row">
-                  {/* {allNotes.length === 0 && (
-                    <p
-                      style={{
-                        color: "red",
-                        fontSize: "20px",
-                        textAlign: "center",
-                      }}
-                    >
-                      You have not added any Todos yet..!
-                    </p>
-                  )} */}
+                  {allNotes.length === 0 && "You have not added any Todos yet..!"}
                   {loading ? <Loader /> : <></>}
-                  {allNotes.map((note, index) => (
-                    <div className="col-md-3">
-                      <div className="card" style={{ width: "200px" }}>
-                        <div className="card-body">
-                          <h4 className="card-title">
-                            <strong>#{index + 1}</strong>
-                          </h4>
-                          <br />
-                          <p>{note.todoNote}</p>
-                          <button
-                            type="button"
-                            className="btn btn-danger"
-                            // onClick={() => deleteNote(note.id)}
-                          >
-                            <span class="glyphicon glyphicon-trash"></span>
-                          </button>
-                          &nbsp;&nbsp;&nbsp;
-                          {/* <button
+                  {allNotes.length > 0 &&
+                    allNotes.map((note, index) => (
+                      <div className="col-md-3">
+                        <div className="card" style={{ width: "200px" }}>
+                          <div className="card-body">
+                            <h4 className="card-title">
+                              <strong>#{index + 1}</strong>
+                            </h4>
+                            <br />
+                            <p>{note.todoNote}</p>
+                            <button
+                              type="button"
+                              className="btn btn-danger"
+                              onClick={() => deleteNote(note.todo_Id)}
+                            >
+                              <span class="glyphicon glyphicon-trash"></span>
+                            </button>
+                            &nbsp;&nbsp;&nbsp;
+                            {/* <button
                             type="button"
                             className="btn btn-primary"
                             //   onClick={() => updateNote(note.id)}
@@ -167,10 +170,10 @@ const TodoEntryNormal = () => {
                           >
                             <span class="glyphicon glyphicon-pencil"></span>
                           </button> */}
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
                   <br />
                 </div>
               </div>
@@ -178,7 +181,6 @@ const TodoEntryNormal = () => {
           </div>
         </div>
       </div>
-      {loading ? <Loader /> : <></>}
     </div>
   );
 };
--- src/constants/endPoints.js
+++ src/constants/endPoints.js
@@ -1,9 +1,15 @@
 // export const NOTES_API = "http://localhost:3001/notes/";
 // export const USERS_API = "http://localhost:3002/users/";
 
-//export const NOTES_API = "http://localhost/php-api/getNotes.php";
-//export const ADD_NOTE_API = "http://localhost/php-api/addNote.php";
+
+/* Local API */ 
+// export const NOTES_API = "http://localhost/php-api/getNotes.php";
+// export const ADD_NOTE_API = "http://localhost/php-api/addNote.php";
+// export const DELETE_NOTE_API="http://localhost/php-api/deleteNote.php"
 //  export const USERS_API = "http://localhost/php-api/userLogin.php";
 
+
+/* Public API */
 export const NOTES_API ="https://rnk-projects.000webhostapp.com/php-api/getNotes.php";
 export const ADD_NOTE_API ="https://rnk-projects.000webhostapp.com/php-api/addNote.php";
+export const DELETE_NOTE_API="https://rnk-projects.000webhostapp.com/php-api/deleteNote.php"
